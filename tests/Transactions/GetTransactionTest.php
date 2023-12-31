de60e7badeee056869fc12c83e21059dca24b7f7
Kepsondiaz <kherabadiaby328@gmail.com>
2023-12-30 20:12:38 -0500

    test functions

---
+++ tests/Transactions/GetTransactionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dexchange\Client\tests\Transactions;
+
+use Mockery;
+use Dexchange\Client\Options;
+use Dexchange\Client\DexchangeSdk;
+
+it('should get transaction', function() {
+    $options = new Options('production');
+    $apikey = '8fcdca23474b7d2612534df';
+    $dexchangeSdk = new DexchangeSdk($apikey, $options);
+
+    $dexchangeSdkMock = Mockery::mock('Transactions', ['alalSdk' => $dexchangeSdk]);
+
+    $dexchangeSdkMock->shouldReceive('getTransaction')
+    ->with('transactionId')
+    ->andReturn(['data' => [123, '2023-11-07T05:31:56Z', '2023-11-07T05:31:56Z', 'Number', 'ServiceCode', 'ServiceName', 'status'], "message" => "get transaction", "statusCode" => "200"]);
+
+    $result =  $dexchangeSdkMock->getTransaction('transactionId');
+
+    expect($result)->toBe(['data' => [123, '2023-11-07T05:31:56Z', '2023-11-07T05:31:56Z', 'Number', 'ServiceCode', 'ServiceName', 'status'], "message" => "get transaction", "statusCode" => "200"]);
+
+});
---
+++ tests/Transactions/InitTransactionTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dexchange\Client\tests\Transactions;
+
+use Mockery;
+use Dexchange\Client\Options;
+use Dexchange\Client\DexchangeSdk;
+
+it('should initialise new transaction', function() {
+    $options = new Options('production');
+    $apikey = '8fcdca23474b7d2612534df';
+    $dexchangeSdk = new DexchangeSdk($apikey, $options);
+
+    $dexchangeSdkMock = Mockery::mock('Transactions', ['alalSdk' => $dexchangeSdk]);
+
+    $dexchangeSdkMock->shouldReceive('initTransaction')
+    ->with(123, 'callBackURL', 'externalTransactionId', 'failureUrl', '20231228', 'serviceCode', 'successUrl')
+    ->andReturn(['data' => [123, 'callBackURL', 'cashout_url', 'externalTransactionId', 'failureUrl', 'number', 'status', true, 'successUrl', 123, 'transactionId', 'transactionType', 'webhook'], "message" => "transaction initialise", "statusCode" => "200"]);
+
+    $result =  $dexchangeSdkMock->initTransaction(123, 'callBackURL', 'externalTransactionId', 'failureUrl', '20231228', 'serviceCode', 'successUrl');
+
+    expect($result)->toBe(['data' => [123, 'callBackURL', 'cashout_url', 'externalTransactionId', 'failureUrl', 'number', 'status', true, 'successUrl', 123, 'transactionId', 'transactionType', 'webhook'], "message" => "transaction initialise", "statusCode" => "200"]);
+
+
+});
